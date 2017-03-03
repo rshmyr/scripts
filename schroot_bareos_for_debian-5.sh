2743cc94de49ce479a46323f45c10f1e02ad4321
Roman Shmyr EPAM <romans@nl-wag-epam01.nl.meteogroup.net>
2017-03-03 15:55:16 +0000

    the 1-st modification

--- schroot_bareos_for_debian-5.sh
+++ schroot_bareos_for_debian-5.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+arc=$(uname -m)
+
+if [ "$arc" == "x86_64" ]; then
+ ARCH="amd64"
+else
+ ARCH="i386"
+fi
+
 echo -e "deb http://ftp.de.debian.org/debian-archive/debian/ lenny main non-free contrib\ndeb-src http://ftp.de.debian.org/debian-archive/debian/ lenny main non-free contrib" > /etc/apt/sources.list
 
 apt-get update
@@ -23,19 +31,19 @@ cp /etc/schroot/schroot.conf /etc/schroot/schroot.conf_orig
 echo -e '[wheezy-test]\ndescription=CHROOT for BAREOS Agent\ntype=directory\nlocation=/chroot-wheezy\nusers=root' >> /etc/schroot/schroot.conf
 
 cd /chroot-wheezy/tmp/
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-bconsole_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-client_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-common_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-filedaemon_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/libfastlz_0.1-7.2_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/libfastlz-dev_0.1-7.2_i386.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-bconsole_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-client_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-common_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-filedaemon_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/libfastlz_0.1-7.2_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/libfastlz-dev_0.1-7.2_$ARCH.deb
 
 cd /tmp/
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-bconsole_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-client_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-common_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/bareos-filedaemon_16.2.4-12.1_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/libfastlz_0.1-7.2_i386.deb
-wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/i386/libfastlz-dev_0.1-7.2_i386.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-bconsole_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-client_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-common_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/bareos-filedaemon_16.2.4-12.1_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/libfastlz_0.1-7.2_$ARCH.deb
+wget -N http://download.bareos.org/bareos/release/16.2/Debian_7.0/$ARCH/libfastlz-dev_0.1-7.2_$ARCH.deb
 
 /usr/bin/schroot -c wheezy-test "/root/tester.sh"
